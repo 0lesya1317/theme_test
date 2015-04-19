fba6674733f39647aba7c375f377b3742cd16f4e
Olesya <olesya@yaremenko.ws>
2015-04-19 18:59:34 +0300

    first_commit

---
+++ test_js_code.js
@@ -0,0 +1,23 @@
+/**
+ @author jane;
+*/
+
+/*
+some text here
+*/
+
+//another comment
+boolean right = 'true';
+boolean wrong = "false";
+
+const int = 9;
+double = 2.5 + 3.0;
+
+var regExp1 = /w3schools/i;
+var regExp2 = /microsoft/i;
+
+Music.play = function(track){
+	if(track != ""){
+	start_playing(track);
+	}
+};
